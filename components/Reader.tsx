bcd0e44ce24d9f00fd15d6fa9dbc45662b5603cd
Jay Harris <jay.harris@outlook.co.nz>
2021-01-26 17:44:31 +0100

    Working book display

--- components/Reader.tsx
+++ components/Reader.tsx
@@ -1,21 +1,29 @@
 import * as Epub from "epubjs/dist/epub"
-import { useEffect, useMemo, useRef } from "react"
-import dynamic from 'next/dynamic'
+import type { Book, Rendition } from 'epubjs';
+import { useEffect, useMemo, useRef, useState } from "react"
 
 interface Props {
     bookUrl: string;
 }
 
 export default function Reader(props: Props) {
-    const rootRef = useRef<HTMLDivElement>();
-    const book = useMemo(() => process.browser ? new Epub.Book(props.bookUrl) : null, [props.bookUrl]);
+    const bookElRef = useRef<HTMLDivElement>();
+    const book = useMemo(() => process.browser ? new Epub.Book(props.bookUrl) : null, [props.bookUrl]) as Book;
+    const [rendition, setRendition] = useState<Rendition>(null);
+
     useEffect(() => {
         if (!book) return;
         
-        const rendition = book.renderTo(rootRef.current);
+        const rendition = book.renderTo(bookElRef.current);
+        setRendition(rendition);
         rendition.display();
     }, [book])
-    return <div ref={rootRef} className="w-screen h-screen bg-red-800">
+    return <div className="w-screen h-screen bg-red-800">
+        <div className="absolute top-0 bottom-0 left-0 right-0" ref={bookElRef}>
 
+        </div>
+        <div className="absolute top-0 bottom-0 left-0 right-0" onClick={() => rendition && rendition.next()}>
+
+        </div>
     </div>
 }
