1b8f9b1e2c1d8ba902a19df8216f3551091406cb
Jay Harris <jay.harris@outlook.co.nz>
2021-02-05 17:16:55 +0100

    Hide overflowing book title

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default (props: Props) => {
 
     return <Link href={`/read/${encodeURIComponent(props.metadata.bookId)}`}>
-        <div className="rounded-md shadow-lg px-2 py-1 w-32 h-48 border-gray-200 border cursor-pointer">
+        <div className="rounded-md shadow-lg px-2 py-1 w-32 h-48 border-gray-200 border cursor-pointer overflow-hidden">
             <h2 className="font-semibold">{props.metadata.title}</h2>
             by <span className="italic">{props.metadata.creator}</span>
         </div>
