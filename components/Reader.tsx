991f14e9f01e8029e535cbc29575e807ba7c8b4a
Jay Harris <jay.harris@outlook.co.nz>
2021-10-28 16:38:11 +0200

    Formatting

--- components/Reader.tsx
+++ components/Reader.tsx
@@ -1,11 +1,8 @@
-import type { Rendition } from 'epubjs';
-import React, { useEffect, useRef, useState } from "react";
-import { getBookProgress } from "../data/book";
+import React, { useRef } from "react";
 import useBookInteractions from '../hooks/useBookInteractions';
 import useBookPercentage from "../hooks/useBookPercentage";
 import { useBook } from "../hooks/usePromise";
 import useRendition from '../hooks/useRendition';
-import { addSwipeEmitter, addTapEmitter } from '../utils/gestures';
 import ProgressBar from "./ProgressBar";
 import StoreBookProgress from "./StoreBookProgress";
 
@@ -16,13 +13,14 @@ interface Props {
 export default function Reader(props: Props) {
     const bookElRef = useRef<HTMLDivElement>();
     const book = useBook(props.id);
+
     const rendition = useRendition(book, bookElRef);
     globalThis.rendition = rendition;
     globalThis.book = book;
 
-    const progress = useBookPercentage(rendition);
-
     useBookInteractions(rendition, bookElRef);
+
+    const progress = useBookPercentage(rendition);
 
     return <div className="w-screen h-screen">
         <StoreBookProgress rendition={rendition} />
