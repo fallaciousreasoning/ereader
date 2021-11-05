518a7ee07b35a73fa0be829a858ebedac5189ec0
Jay Harris <jay.harris@outlook.co.nz>
2021-11-05 15:28:20 +0100

    Begin work on settings for reading

---
+++ pages/settings.tsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import React from "react";
+import { updateTheme, useTheme } from "../data/theme";
+
+const themeModes = ['light', 'dark', 'system'] as const;
+export default function Settings() {
+    const theme = useTheme();
+    return <div>
+        <Head>
+            <title>Settings</title>
+        </Head>
+        <div className="flex flex-col m-auto my-2 max-w-3xl p-2 rounded shadow gap-2">
+            <h2 className="text-2xl font-bold">Settings</h2>
+            <div className="flex flex-col">
+                <h3 className="text-lg">Theme</h3>
+                <select className="max-w-xs" value={theme.mode ?? 'system'} onChange={e => updateTheme({ mode: e.target.value as any })}>
+                    {themeModes.map(m => <option key={m} value={m}>{m}</option>)}
+                </select>
+            </div>
+            <div className="flex flex-col">
+                <h3 className="text-lg">Font Size</h3>
+                <div className="flex gap-2 w-full max-w-xs">
+                    <input className="flex-1" type="range" min={8} max={32} value={theme.fontSize} onChange={(e) => updateTheme({ fontSize: parseInt(e.target.value) ?? 18 })} />
+                    <span>{theme.fontSize}pt</span>
+                </div>
+            </div>
+            <div className="flex flex-col">
+                <h3 className="text-lg">Background Color</h3>
+                <div className="flex gap-2 w-full">
+                    <input className="max-w-xs flex-1" type="color" value={theme.background} onChange={(e) => updateTheme({ background: e.target.value })} />
+                </div>
+            </div>
+            <div className="flex flex-col">
+                <h3 className="text-lg">Text Color</h3>
+                <div className="flex gap-2 w-full">
+                    <input className="max-w-xs flex-1" type="color" value={theme.text} onChange={(e) => updateTheme({ text: e.target.value })} />
+                </div>
+            </div>
+        </div>
+    </div>
+}
