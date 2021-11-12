0275542b2751b26819bfd0ef8ed7b22b56aa1973
Jay Harris <jay.harris@outlook.co.nz>
2021-11-12 17:16:32 +0100

    Add a theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import Head from "next/head";
+import React from "react";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#90cdf4" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
