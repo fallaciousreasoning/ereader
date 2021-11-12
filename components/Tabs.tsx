bf2ea341c25b600baf6ae354663d97025a23a579
Jay Harris <jay.harris@outlook.co.nz>
2021-11-12 13:02:12 +0100

    Better tab borders

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -20,17 +20,29 @@ function RenderTabHeader(selected: boolean, header: TabHeader) {
     return header;
 }
 
+const tabBorderThickness = '1px';
+const verticalSeparator = { width: tabBorderThickness };
+const horizontalSeparator = { height: tabBorderThickness };
+function RenderTabHeaderContainer(props: { selected: boolean, setSelected: (s: boolean) => void, header: TabHeader }) {
+    return <div className={`p-2 relative`} onClick={e => {
+        props.setSelected(true);
+        e.stopPropagation();
+    }}>
+        {props.selected && <div style={verticalSeparator} className="absolute left-0 top-0 bottom-0 w-1 bg-black" />}
+        {RenderTabHeader(props.selected, props.header)}
+        {props.selected && <div style={verticalSeparator} className="absolute right-0 top-0 bottom-0 w-1 bg-black" />}
+        {!props.selected && <div style={horizontalSeparator} className="absolute right-0 left-0 bottom-0 h-1 bg-black" />}
+    </div>
+}
+
 export default function Tabs(props: Props) {
     const [selected, setSelected] = useState(0);
     const selectedChild = props.children[selected];
     return <div className="w-full flex flex-col">
         <div className={`flex flex-row bg-background ${props.stickyHeader ? 'sticky top-0' : ''}`}>
-            {props.headers.map((h, i) => <div key={i} className={`${i === selected ? 'bg-primary' : ''} p-2`} onClick={e => {
-                setSelected(i);
-                e.stopPropagation();
-            }}>
-                {RenderTabHeader(i === selected, h)}
-            </div>)}
+            <div className="w-1 border-solid border-black border-b" />
+            {props.headers.map((h, i) => <RenderTabHeaderContainer selected={i === selected} setSelected={() => setSelected(i)} header={h} />)}
+            <div className="w-1 border-solid border-black border-b flex-1" />
         </div>
         <div className="px-2 overflow-auto">
             {selectedChild}
