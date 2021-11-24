b1ccb9ce4189a99e555e47e9dc057cfc3be823f5
Jay Harris <jay.harris@outlook.co.nz>
2021-11-24 15:47:59 +0100

    Working middle n func

--- utils/stats.ts
+++ utils/stats.ts
@@ -11,27 +11,13 @@ export const median = (items: number[]) => {
 }
 
 export const middleN = <T>(items: T[], n: number) => {
-    const center = Math.floor(items.length/2);
+    const start = Math.max(0, Math.floor(items.length/2) - Math.floor(n/2));
 
     const result: T[] = [];
-    for (let i = 0; i < Math.floor(n/2); ++i) {
-        const low = center - i;
-        const high = center + i;
-
-        // First iteration, both are zero. This should pick the middle item (if odd), or the left hand center item (if even).
-        if (low !== high && high < items.length) {
-            result.push(items[high]);
-        }
-
-        // Otherwise, we grow evenly, biased one towards the left.
-        if (low >= 0)
-            result.push(items[low]);
+    for (let i = start; i < start + n; ++i) {
+        if (i >= items.length) break;
+        result.push(items[i]);
     }
-
-    // If we need one more item (because the center rounded down), push the next right hand item.
-    const lastRight = center + Math.ceil(n/2);
-    if (result.length < n && lastRight < items.length)
-        result.push(items[lastRight]);
     return result;
 }
 
